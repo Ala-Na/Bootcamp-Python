eb017826781fb70404921e21c45d1c522eba80e3
anadege <ilmefautuneadresse@gmail.com>
2021-11-11 21:44:08 +0100

    go to d03

---
+++ D03/ex00/NumPyCreator.py
@@ -0,0 +1,10 @@
+class   NumPyCreator():
+
+    def from_list(lst):
+    #takes a list or nested list and returns its corresponding Numpy array
+
+    def from_tuple(tlp):
+    #takes a tuple or nested tuple and returns its corresponding Numpy array
+
+    def from_iterable(itr):
+    #
