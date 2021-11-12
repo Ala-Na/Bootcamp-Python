31d5096981bb49c27d919e9169d9392b2857fa0e
anadege <ilmefautuneadresse@gmail.com>
2021-11-12 18:51:27 +0100

    started d03

--- D03/ex00/NumPyCreator.py
+++ D03/ex00/NumPyCreator.py
@@ -1,10 +1,61 @@
+import numpy
+import random
+
 class   NumPyCreator():
 
-    def from_list(lst):
-    #takes a list or nested list and returns its corresponding Numpy array
+    def from_list(self, lst, *args):
+        if not args and isinstance(lst, list):
+            dtype=object
+            if isinstance(lst[0], list):
+                size = -1
+                for row in lst:
+                    if size == -1:
+                        size = len(row)
+                    if len(row) != size:
+                        print("from_list received list containing elements of differents sizes.")
+                        return None
+                    if any(isinstance(elem, str) for elem in row):
+                        dtype='<U21'
+            if any(isinstance(value, str) for value in lst):
+                dtype='<U21'
+            array = numpy.asarray(lst, dtype=dtype)
+            return array
+        else:
+            print("from_list received a non list argument.")
+            return None
 
-    def from_tuple(tlp):
-    #takes a tuple or nested tuple and returns its corresponding Numpy array
+    def from_tuple(self, tpl):
+        if isinstance(tpl, tuple):
+            return numpy.asarray(tpl)
+        else:
+            print("from_tuple received a non tuple argument.")
+            return None
 
-    def from_iterable(itr):
-    #
+    def from_iterable(self, itr):
+        try:
+            iterator = iter(itr)
+            return numpy.fromiter(itr, int)
+        except:
+            print("from_iterable received a non iterable argument.")
+            return None
+
+    def from_shape(self, shape, value = 0):
+        if isinstance(shape, tuple):
+            return numpy.full(shape, value)
+        else:
+            print("from_shape received a non tuple argument.")
+            return None
+
+    def random(self, shape):
+        if isinstance(shape, tuple):
+            return numpy.random.rand(*shape)
+        else:
+            print("random received a non tuple argument.")
+            return None
+
+    def identity(self, n):
+        if isinstance(n, int):
+            return numpy.identity(n)
+        else:
+            print("identity received a non integer argument.")
+            return None
---
+++ D03/ex00/test.py
@@ -0,0 +1,25 @@
+from NumPyCreator import NumPyCreator
+
+npc = NumPyCreator()
+
+lst = [[1,2,3],[6,3,4]]
+print(npc.from_list(lst))
+print("")
+print(npc.from_list([[1,2,3],[6,4]]))
+print("")
+print(npc.from_list([[1,2,3],['a','b','c'],[6,4,7]]))
+print("")
+print(npc.from_list((1,2),(3,4)))
+print("")
+print(npc.from_tuple(("a","b","c")))
+print("")
+print(npc.from_tuple([[1,2,3],[6,3,4]]))
+print("")
+print(npc.from_iterable(range(5)))
+print("")
+shape=(3,5)
+print(npc.from_shape(shape))
+print("")
+print(npc.random(shape))
+print("")
+print(npc.identity(4))
