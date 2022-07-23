f0103c062ab416a005dd2d1dd39a62095b14a1e5
Nadege ALAVOINE <anadege@e1r3p1.clusters.42paris.fr>
2022-07-23 20:20:27 +0200

    D01/ex02 cleaned

--- D01/ex02/test.py
+++ D01/ex02/test.py
@@ -11,9 +11,11 @@ def main():
     print('Vector 1 tranposed:', v1.T())
 
     v2 = v1 * 5
-    print('Vector 2 (v1 * 5):', v2)
+    print('Vector 1 * 5:', v2)
     # Expected output:
     # Vector([[0.0], [5.0], [10.0], [15.0]])
+
+    print("")
 
     # Row vector of shape 1 * n
     v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
@@ -21,12 +23,14 @@ def main():
     print('Vector 1 tranposed:', v1.T())
 
     v2 = v1 * 5
-    print('Vector 2 (v1 * 5):', v2)
+    print('Vector 1 * 5:', v2)
     # Expected output
     # Vector([[0.0, 5.0, 10.0, 15.0]])
 
+    print("")
+
     v2 = v1 / 2.0
-    print('div v2:', v2)
+    print('Vector 1 / 2.0:', v2)
     # Expected output
     # Vector([[0.0], [0.5], [1.0], [1.5]])
 
@@ -38,56 +42,77 @@ def main():
     # Expected output:
     # NotImplementedError: Division of a scalar by a Vector is not defined here.
 
+    print("")
+
     # Column vector of shape (n, 1)
-    print(Vector([[0.0], [1.0], [2.0], [3.0]]).shape)
+    print("Shape of n * 1:", Vector([[0.0], [1.0], [2.0], [3.0]]).shape)
     # Expected output
     # (4,1)
-    print(Vector([[0.0], [1.0], [2.0], [3.0]]).values)
+    print("Values of n * 1:", Vector([[0.0], [1.0], [2.0], [3.0]]).values)
     # Expected output
     # [[0.0], [1.0], [2.0], [3.0]]
     # Row vector of shape (1, n)
-    print(Vector([[0.0, 1.0, 2.0, 3.0]]).shape)
+    print("Shape of 1 * n:", Vector([[0.0, 1.0, 2.0, 3.0]]).shape)
     # Expected output
     # (1,4)
-    print(Vector([[0.0, 1.0, 2.0, 3.0]]).values)
+    print("Values of 1 * n:", Vector([[0.0, 1.0, 2.0, 3.0]]).values)
     # Expected output
     # [[0.0, 1.0, 2.0, 3.0]]
 
+    print("")
+
     # Example 1:
     v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
-    print(v1.shape)
+    print("Vector 1 :", v1)
+    print("Vector 1 shape:", v1.shape)
     # Expected output:
     (4,1)
-    print(v1.T())
+    print("Tranposed:", v1.T())
     # Expected output:
     # Vector([[0.0, 1.0, 2.0, 3.0]])
-    print(v1.T().shape)
+    print("Transposed shape:", v1.T().shape)
     # Expected output:
     # (1,4)
+
+    print("")
+
     # Example 2:
     v2 = Vector([[0.0, 1.0, 2.0, 3.0]])
-    print(v2.shape)
+    print("Vector 1 :", v2)
+    print("Vector 2 shape:", v2.shape)
     # Expected output:
     # (1,4)
-    print(v2.T())
+    print("Tranposed:", v2.T())
     # Expected output:
     # Vector([[0.0], [1.0], [2.0], [3.0]])
-    print(v2.T().shape)
+    print("Transposed shape:", v2.T().shape)
     # Expected output:
     # (4,1)
+
+    print("")
 
     # Example 1:
     v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
     v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
-    print(v1.dot(v2))
+    print('Vector 1:', v1)
+    print("Vector 2:", v2)
+    print('Dot product vector 1 by vector 2:', v1.dot(v2))
     # Expected output:
     # 18.0
+
+    print("")
     v3 = Vector([[1.0, 3.0]])
     v4 = Vector([[2.0, 4.0]])
-    print(v3.dot(v4))
+    print('Vector 3:', v3)
+    print("Vector 4:", v4)
+    print('Dot product vector 3 by vector 4:', v3.dot(v4))
     # Expected output:
     # 13.0
-    v1
+    # /!\ 14.0 and not 13.0 as subject because 1*2 + 4*3 = 14
+
+    print("")
+    print(repr(v1))
+    # Need to call repr function and not just v1 as suggested by subject
     # Expected output: to see what __repr__() should do
     # [[0.0, 1.0, 2.0, 3.0]]
     print(v1)
--- D01/ex02/vector.py
+++ D01/ex02/vector.py
@@ -15,7 +15,7 @@ class   Vector:
     def T(self):
         new_values = []
         if self.shape[0] == 1:
-            for row in self.vector_list:
+            for row in self.values:
                 for column in row:
                     new_values.append([column])
         else:
@@ -46,7 +46,7 @@ class   Vector:
 
     def __sub__(self, other):
         if not isinstance(other, Vector):
-          raise NotImplementedError("Vectors can only be substracted with other vectors.")
+            raise NotImplementedError("Vectors can only be substracted with other vectors.")
         if len(self.values) != len(other.values):
             raise TypeError("Can't substract two vectors of differents dimensions.")
         elif type(self.values[0]) != type(other.values[0]):
@@ -68,33 +68,25 @@ class   Vector:
         if other == 0:
             raise ZeroDivisionError("Can't divide by a zero !")
         new_values = []
-        if isinstance(self.values[0], float):
-            for elem in self.values:
-                new_values.append(elem / other)
-        else:
-            for elem in self.values:
-                for sub_elem in elem:
-                    sub_new_values = []
-                    sub_new_values.append(sub_elem / other)
-                    new_values.append(sub_new_values)
+        for row in self.values:
+            sub_new_values = []
+            for col in row:
+                sub_new_values.append(col / other)
+            new_values.append(sub_new_values)
         return Vector(new_values)
 
     def __rtruediv__(self, other):
-        raise NotImplementedError("Can't divide by vector.")
+        raise ArithmeticError("Can't divide by vector.")
 
     def __mul__(self, other):
-        if not isinstance(other, int) or (isinstance(other, float) and not other.is_integer()):
+        if not (isinstance(other, int) or isinstance(other, float)):
             raise NotImplementedError("Only multiplication vectors with scalars are handled.")
         new_values = []
-        if isinstance(self.values[0], float) or len(self.values[0]) == 1:
-            for elem in self.values:
-                new_values.append(elem * other)
-        else:
-            for elem in self.values:
-                for sub_elem in elem:
-                    sub_new_values = []
-                    sub_new_values.append(sub_elem * other)
-                    new_values.append(sub_new_values)
+        for row in self.values:
+            sub_new_values = []
+            for col in row:
+                sub_new_values.append(col * other)
+            new_values.append(sub_new_values)
         return Vector(new_values)
 
     def __rmul__(self, other):
@@ -102,12 +94,10 @@ class   Vector:
 
     def __str__(self):
         txt = "{}".format(self.values)
-        txt += "\t"
-        txt += "{}".format(self.shape)
         return txt
 
     def __repr__(self):
-        print("{}".format(self.__str__()))
+        return self.__str__()
 
     def dot(self, other):
         if not isinstance(other, Vector):
@@ -125,19 +115,3 @@ class   Vector:
                 for sub_elem, sub_elem_ot in zip(elem, elem_ot):
                     res += sub_elem * sub_elem_ot
         return res
-
-
-
-
-
-
-#Column vectors : list of list of float
-#Row vectors : list of float
-#Shape for column : (nb_elem, 1)
-#Shape for row : (1, nb_elem)
-
-#Initialization :
-#- List of float [f, f, f, f] (row)
-#- List of lists of floats [[f], [f], [f], [f]] (column)
-#- Size Vector(size) => [[f], [f], [f]] (column)
-#- Range Vector((start, end)) => [[start], [f], [f], [end]] (column)
